64b805c00dfcdeffb7ff3843c38c32ed811a415e
Moon <59304977+mun9769@users.noreply.github.com>
2023-05-22 10:15:05 +0900

    1043.거짓말

--- TEAM A/Week 4/Eren/1043.거짓말/거짓말.cpp
+++ TEAM A/Week 4/Eren/1043.거짓말/거짓말.cpp
@@ -1,98 +1,63 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <map>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n, m;
-bool arr[60];
-vector<vector<int>> v(60);
-vector<vector<int>> map(60);
-int result;
-int unf[55];
-vector<int> vv;
-bool check[60];
+bool chk[55];
+int p[55];
+int n, m, k;
+vector<int> party[51];
 
-int Find(int v)
-{
-	if (v == unf[v]) return unf[v];
-	else return Find(unf[v]);
+int Find(int x){
+  if(p[x] == x) return x;
+  else return p[x] = Find(p[x]);
 }
 
-void Union(int a, int b)
-{
-	a = Find(a);
-	b = Find(b);
-	if (a != b) unf[a] = b;
+bool Union(int x, int y){
+  x = Find(x);
+  y = Find(y);
+  if(x == y) return false;
+
+  if(chk[y] == true) swap(x, y);
+  p[y] = x;
+  return true;
 }
 
-void Infect(int a)
-{
-	if (arr[a] == false) {
-		arr[a] = true;
-		for (int i = 1; i <= n; i++)
-		{
-			if (unf[i] == a)
-				Infect(i);
-		}
-		Infect(unf[a]);
-	}
-}
+int main(){
+  cin.tie(0) -> ios_base::sync_with_stdio(0);
+  cin >> n >> m;
+  cin >> k;
+  for(int i=1; i<=k; i++){
+    int a; 
+    cin >> a;
+    chk[a] = true;
+  }
+  for(int i=1; i<=m; i++){ 
+    int num;// 변수명 뭐라할까.
+    cin >> num;
+    for(int j=1; j<=num; j++){
+      int id;
+      cin >> id;
+      party[i].push_back(id);
+    }
+  }
+  for(int i=1; i<=50; i++) p[i] = i;
 
+  for(int i=1; i<=m; i++){
+    for(int j=0; j<party[i].size()-1; j++){
+      int prev = party[i][j];
+      int nxt = party[i][j+1];
+      Union(prev, nxt);
+    }
+  }
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin >> n >> m;
-	result = m;
+  int ans = m;
 
-	for (int i = 1; i <= n; i++)
-		unf[i] = i;
+  for(int i=1; i<=m; i++){
+    for(int j=0; j<party[i].size(); j++){
+      int id = party[i][j];
+      int root = Find(id);
+      if(chk[root]) { ans--; break; }
+    }
+  }
 
-	int known = 0;
-	cin >> known;
-	for (int i = 1; i <= known; i++) {
-		int num;
-		cin >> num;
-		vv.push_back(num);
-	}
-
-	for (int i = 1; i <= m; i++)
-	{
-		int personCount;
-		cin >> personCount;
-		int prev;
-		for (int j = 1; j <= personCount; j++) 
-		{
-			int num;
-			cin >> num;
-			v[i].push_back(num);
-			if (j >= 2) {
-				Union(prev, num);
-			}
-			prev = num;
-		}
-	}
-
-	for (int i = 0; i < vv.size(); i++)
-	{
-		if (arr[vv[i]] == true)
-			continue;
-		Infect(vv[i]);
-	}
-
-	for (int i = 1; i <= m; i++)
-	{
-		for (int j = 0; j < v[i].size(); j++)
-		{
-			if (arr[v[i][j]])
-			{
-				result--;
-				break;
-			}
-		}
-	}
-
-	cout << result;
-
+  cout << ans;
 }
